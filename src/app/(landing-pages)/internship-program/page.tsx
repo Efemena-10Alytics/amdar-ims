f424b790625fc30e4e68b048900d452d1ad0e28d
Adaji Mukhtar <alfadaji@gmail.com>
2026-01-22 06:11:44 +0100

    started working on the hero page

--- src/app/(landing-pages)/internship-program/page.tsx
+++ src/app/(landing-pages)/internship-program/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import InternshipHero from '@/components/_core/landing-pages/internship-program/hero'
 
 const InternShipProgram = () => {
   return (
     <div>
+      <InternshipHero />
     </div>
   )
 }
