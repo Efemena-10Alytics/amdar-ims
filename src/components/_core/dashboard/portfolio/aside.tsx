244c12945ff6fe21379d27752d1426a06d1ac575
Adaji Mukhtar <alfadaji@gmail.com>
2026-03-15 21:27:25 +0100

    readjusted the responsiveness of dashboard layout

--- src/components/_core/dashboard/portfolio/aside.tsx
+++ src/components/_core/dashboard/portfolio/aside.tsx
@@ -19,7 +19,7 @@ type AsideProps = {
 
 const Aside = ({ step, onStepChange }: AsideProps) => {
   return (
-    <aside className="lg:w-75 shrink-0 flex flex-col">
+    <aside className="w-full lg:max-w-75 shrink-0 flex flex-col">
       <nav className="rounded-xl w-full bg-[#F8FAFC] p-4" aria-label="Portfolio steps">
         <ul className="space-y-1 flex flex-row lg:flex-col overflow-x-auto">
           {STEPS.map((s, i) => {
