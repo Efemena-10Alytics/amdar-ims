8e963b4e03822f187b96079c584786efa4166cbe
Adaji Mukhtar <alfadaji@gmail.com>
2026-04-17 06:21:30 +0100

    refactored experience hover effect and commented out sidebar footer

--- src/components/_core/dashboard/portfolio/template/classic/my-work-experince.tsx
+++ src/components/_core/dashboard/portfolio/template/classic/my-work-experince.tsx
@@ -40,18 +40,18 @@ export function MyWorkExperience({
       <div className="text-xl font-semibold text-[#A1A8B1] mb-4">
         My Work Experience
       </div>
-      <div className="">
+      <div className="space-y-4">
         {items.map((item, index) => {
           const itemKey = String(item.id ?? index);
           const isExpanded = expandedItems[itemKey] ?? false;
           return (
             <div key={item.id ?? index}
-              className="w-full"
+              className={cn("w-full", isExpanded ? "bg-[#E8EFF1] pb-2" : "bg-white")}
             >
 
               <button
                 type="button"
-                className={cn("flex p-4 cursor-pointer group text-left transition-colors hover:bg-[#156374] justify-between w-full",
+                className={cn("flex p-4 cursor-pointer group text-left transition-colors justify-between w-full", isExpanded? "": "hover:bg-[#156374]"
                 )}
                 onClick={(e) => {
                   e.stopPropagation();
@@ -62,14 +62,14 @@ export function MyWorkExperience({
               >
                 <div>
                   <div className="flex items-center gap-2 flex-wrap text-left">
-                    <h2 className="text-2xl font-semibold text-[#092A31] transition-colors group-hover:text-white">
+                    <h2 className={cn("text-2xl font-semibold text-[#092A31] transition-colors", isExpanded ? "": "group-hover:text-white")}>
                       {item.company}
                     </h2>
-                    <h3 className="text-sm md:text-base font-semibold text-[#B6CFD4] transition-colors group-hover:text-[#D6E8EC]">
+                    <h3 className={cn("text-sm md:text-base font-semibold text-[#B6CFD4] transition-colors", isExpanded ? "": "group-hover:text-[#D6E8EC]")}>
                       {item.category}
                     </h3>
                   </div>
-                  <p className="mt-0.5 text-sm text-[#64748B] transition-colors group-hover:text-white/90">
+                  <p className={cn("mt-0.5 text-sm text-[#64748B] transition-colors ", isExpanded ? "": "group-hover:text-white")}>
                     {item.role}
                     <span className="mx-1.5" aria-hidden>
                       ·
@@ -79,18 +79,25 @@ export function MyWorkExperience({
 
                 </div>
                 <ArrowUpRight
-                  className={cn("size-5 transition-transform text-[#092A31] group-hover:hidden", isExpanded ? "rotate-45" : "rotate-0")}
+                  className={cn("size-5 transition-transform text-[#092A31] group-hover:hidden",)}
                   aria-hidden
                 />
-                <div className="hidden group-hover:flex text-primary bg-amdari-yellow p-1 rounded-full text-[10px] text-center h-10 w-10 flex items-center justify-center">
-                  {isExpanded ? "Close" : "View"}
+                {isExpanded ? (
+                  <div className="hidden group-hover:flex text-primary bg-white p-1 rounded-full text-[10px] text-center h-10 w-10 items-center justify-center">
+                    <ArrowUpRight
+                      className={cn("size-5 transition-transform text-primary",)}
+                      aria-hidden
+                    />
+                  </div>) : <div className="hidden group-hover:flex text-primary bg-amdari-yellow p-1 rounded-full text-[10px] text-center h-10 w-10 items-center justify-center">
+                  view
                 </div>
+                }
               </button>
 
               {item.descriptions && item.descriptions.length > 0 ? (
                 <div className="">
                   {isExpanded ? (
-                    <ul className="list-disc space-y-3 pl-8 text-sm leading-relaxed text-[#64748B] marker:text-[#64748B]">
+                    <ul className="list-disc space-y-3 pl-8 text-sm leading-relaxed text-[#092A31] marker:text-[#64748B]">
                       {item.descriptions.map((description, descriptionIndex) => (
                         <li className="capitalize" key={`${item.id ?? index}-desc-${descriptionIndex}`}>
                           {description}
