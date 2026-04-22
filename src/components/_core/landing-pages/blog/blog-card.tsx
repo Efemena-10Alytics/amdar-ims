9f4cb4f317c2d000e76ce603868808ca7e6daafc
Adaji Mukhtar <alfadaji@gmail.com>
2026-04-22 16:23:43 +0100

    removed the IWD terms modal and adjusted hover for blog card

--- src/components/_core/landing-pages/blog/blog-card.tsx
+++ src/components/_core/landing-pages/blog/blog-card.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react";
-import {  imageStorageUrl,  } from "@/lib/utils";
+import { imageStorageUrl, } from "@/lib/utils";
 
 export type BlogCardData = {
     id: string | number;
@@ -15,36 +15,38 @@ export default function BlogCard({ post }: { post: BlogCardData }) {
     const blogImageUrl = `${imageStorageUrl}/images/${post.image}`;
     console.log(blogImageUrl);
     return (
-        <article className="space-y-3 cursor-pointer hover:p-3 w-full hover:bg-[#E8EFF1] group rounded-lg duration-300 transition-all">
-            <div className="overflow-hidden rounded-lg bg-[#E8EFF1]">
-                <img
-                    src={blogImageUrl}
-                    alt={post.title}
-                    className="h-44 w-full object-cover"
-                />
-            </div>
-            <div className="space-y-3">
-                <div className="flex items-center gap-3 text-xs font-medium uppercase text-[#8EA0AA]">
-                    <span>{post.category}</span>
-                    <span className="normal-case">{post.date}</span>
+        <Link href={post.href}>
+            <article className="space-y-3 cursor-pointer w-full hover:bg-[#E8EFF1] group rounded-lg duration-300 transition-all">
+                <div className="overflow-hidden rounded-lg bg-[#E8EFF1]">
+                    <img
+                        src={blogImageUrl}
+                        alt={post.title}
+                        className="h-44 w-full object-cover"
+                    />
                 </div>
+                <div className="space-y-3 group-hover:p-3 transition-all duration-300">
+                    <div className="flex items-center gap-3 text-xs font-medium uppercase text-[#8EA0AA]">
+                        <span>{post.category}</span>
+                        <span className="normal-case">{post.date}</span>
+                    </div>
 
-                <h2 className="text-lg font-semibold text-[#092A31] md:text-xl">
-                    {post.title}
-                </h2>
+                    <h2 className="text-lg font-semibold text-[#092A31] md:text-xl">
+                        {post.title}
+                    </h2>
 
-                <div className="flex items-center justify-between pt-1">
-                    <Link
-                        href={post.href}
-                        className="text-base font-medium leading-none text-[#092A31] hover:underline"
-                    >
-                        Read article
-                    </Link>
-                    <span className="inline-flex group-hover:text-primary size-6 items-center justify-center rounded-full bg-[#0E6A76] group-hover:bg-amdari-yellow text-white">
-                        <ArrowUpRight className="size-3.5" />
-                    </span>
+                    <div className="flex items-center justify-between pt-1">
+                        <Link
+                            href={post.href}
+                            className="text-base font-medium leading-none text-[#092A31] hover:underline"
+                        >
+                            Read article
+                        </Link>
+                        <span className="inline-flex group-hover:text-primary size-6 items-center justify-center rounded-full bg-[#0E6A76] group-hover:bg-amdari-yellow text-white">
+                            <ArrowUpRight className="size-3.5" />
+                        </span>
+                    </div>
                 </div>
-            </div>
-        </article>
+            </article>
+        </Link>
     )
 }
