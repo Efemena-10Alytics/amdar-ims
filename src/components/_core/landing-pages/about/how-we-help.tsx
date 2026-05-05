2d077fa6864751353c18bc97dfd3edc9ad19ed9f
Adaji Mukhtar <alfadaji@gmail.com>
2026-05-05 19:26:25 +0100

    added the

--- src/components/_core/landing-pages/about/how-we-help.tsx
+++ src/components/_core/landing-pages/about/how-we-help.tsx
@@ -6,7 +6,7 @@ const HowWeHelp = () => {
   return (
     <section className="bg-[#F6FAFB] py-14 lg:py-20">
       <div className="app-width">
-        <h2 className="mx-auto max-w-[520px] text-center text-4xl leading-tight font-semibold text-[#123943] md:text-5xl">
+        <h2 className="mx-auto max-w-130 text-center text-4xl leading-tight font-semibold text-[#123943] md:text-5xl">
           How We Help <br /> Tech Professionals
         </h2>
 
@@ -16,7 +16,7 @@ const HowWeHelp = () => {
               Internships{" "}
               <span className="text-[#1B8194]">(RAVE by Amdari)</span>
             </h3>
-            <p className="mt-4 max-w-[520px] text-sm leading-relaxed text-[#67828A] md:text-base">
+            <p className="mt-4 max-w-130 text-sm leading-relaxed text-[#67828A] md:text-base">
               Our Career Experience Internship immerses you in a real-world
               work environment, where you collaborate on live projects that
               solve actual business challenges. With mentorship from industry
@@ -35,7 +35,7 @@ const HowWeHelp = () => {
             <h3 className="text-2xl leading-tight font-semibold text-[#123943] md:text-3xl">
               Real-World Projects
             </h3>
-            <p className="mt-4 max-w-[500px] text-sm leading-relaxed text-[#67828A] md:text-base">
+            <p className="mt-4 max-w-125 text-sm leading-relaxed text-[#67828A] md:text-base">
               Our projects go beyond the generic tasks available online,
               offering scenarios with real business context and measurable
               impact. Whether you're a data analyst, engineer, or scientist,
@@ -46,7 +46,7 @@ const HowWeHelp = () => {
         </div>
 
         <article className="mt-12 flex flex-col gap-4 border-t border-[#D9E8EC] pt-8 md:flex-row md:items-start md:justify-between">
-          <div className="max-w-[560px]">
+          <div className="max-w-140">
             <h3 className="text-2xl leading-tight font-semibold text-[#123943] md:text-3xl">
               TalentLoop
             </h3>
