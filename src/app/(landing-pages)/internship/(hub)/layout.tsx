e4a20156b0442abaa4d1bb4021232eb5c5c41d9a
Adaji Mukhtar <alfadaji@gmail.com>
2026-05-11 19:58:34 +0100

    made internship and internship details page server pages and added meta title and description

--- src/app/(landing-pages)/internship/(hub)/layout.tsx
+++ src/app/(landing-pages)/internship/(hub)/layout.tsx
@@ -1,14 +1,3 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: {
-    absolute:
-      "Work experience that helps you land tech jobs easily",
-  },
-  description:
-    "Gain real work experience in Data, Cybersecurity, Project Management, Business Analysis & Product Design",
-};
-
 export default function InternshipHubLayout({
   children,
 }: {
