d752229a18029eadec7cb1968f598c4f41e4b241
Adaji Mukhtar <alfadaji@gmail.com>
2026-05-12 23:56:30 +0100

    started the become partner page

---
+++ src/app/(landing-pages)/business-partners/page.tsx
@@ -0,0 +1,11 @@
+import BusinessPartnersHero from "@/components/_core/landing-pages/business-partners/hero";
+import OutsourcingWork from "@/components/_core/landing-pages/business-partners/outsourcing-work";
+
+export default function BusinessPartnersPage() {
+  return (
+    <main>
+      <BusinessPartnersHero />
+      <OutsourcingWork />
+    </main>
+  );
+}
