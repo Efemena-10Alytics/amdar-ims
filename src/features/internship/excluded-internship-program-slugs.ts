53a882375d6c4fd0b523ab4a04d2a4aa27f7847b
Adaji Mukhtar <alfadaji@gmail.com>
2026-09-08 14:02:00 +0100

    added two program

--- src/features/internship/excluded-internship-program-slugs.ts
+++ src/features/internship/excluded-internship-program-slugs.ts
@@ -5,10 +5,10 @@ export const EXCLUDED_INTERNSHIP_PROGRAM_SLUGS: string[] = [
   "financial-analytics-career-experience-internship-facei",
   "devops-career-experience-internship-dcei",
   "product-design-career-experience-internship-pdcei",
-  "ethical-hacking-career-experience-internship",
+  // "ethical-hacking-career-experience-internship",
   "devops-work-experience-internship-uk-canada",
   "product-design-work-experience-internship-uk-canada",
-  "data-engineering-work-experience-internship-uk-canada",
+  // "data-engineering-work-experience-internship-uk-canada",
   "app-cloud-security-work-experience-internship-uk-canada",
   // "data-engineering-career-experience-internship-decei",
 ];
